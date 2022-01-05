a6b0754028da53354542febd7f90a21e7deda5ff
Guy <guyo0503@gmail.com>
2022-01-05 23:56:15 +0200

    client dataclass

---
+++ client/data_classes.py
@@ -0,0 +1,28 @@
+import os
+from dataclasses import dataclass
+
+
+@dataclass
+class Screen:
+    screen: str = ""
+    prompt: str = ""
+
+
+    def clear_screen(self):
+        os.system('cls')
+
+
+    def prompt_to_screen(self):
+        self.clear_screen()
+        print(self.screen)
+        message = input('Enter Message: ')
+        self.prompt = 'Enter Message: ' + message
+
+        return message
+    
+
+    def print_to_screen(self, message):
+        self.clear_screen()
+        self.screen += (message + '\n')
+        print(self.screen)
+        print(self.prompt)
